4b46002039b50ec27db60c06398ec2194428dd63
Ivan Dvorovy <ivandvorovy@gmail.com>
2026-05-19 23:51:48 +0200

    fixnute recenzie 2.0

--- web/.astro/content-modules.mjs
+++ web/.astro/content-modules.mjs
@@ -1,9 +1,4 @@
 
 export default new Map([
-["src/content/reviews/asd.mdoc", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Freviews%2Fasd.mdoc&astroContentModuleFlag=true")],
-["src/content/reviews/esti-2026-001.mdoc", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Freviews%2Festi-2026-001.mdoc&astroContentModuleFlag=true")],
-["src/content/reviews/ESTI-ALPT-HEAU.mdoc", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Freviews%2FESTI-ALPT-HEAU.mdoc&astroContentModuleFlag=true")],
-["src/content/reviews/opuisdhpgfd.mdoc", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Freviews%2Fopuisdhpgfd.mdoc&astroContentModuleFlag=true")],
-["src/content/reviews/popici-stranka.mdoc", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Freviews%2Fpopici-stranka.mdoc&astroContentModuleFlag=true")],
-["src/content/reviews/styleless.mdoc", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Freviews%2Fstyleless.mdoc&astroContentModuleFlag=true")]]);
+["src/content/reviews/ESTI-ALPT-HEAU.mdoc", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Freviews%2FESTI-ALPT-HEAU.mdoc&astroContentModuleFlag=true")]]);
 		
